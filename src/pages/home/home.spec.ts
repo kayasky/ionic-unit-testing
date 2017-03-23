afb3885f4c9ae87ffcfa53e42073810994b81961
Satbir Jhuti <satbirjhuti@gmail.com>
2017-03-23 15:30:31 -0400

    Add unit tests for home page

---
+++ src/pages/home/home.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { HomePage } from "./home";
+import { IonicModule, NavController } from "ionic-angular/index";
+
+describe('HomePage', function() {
+  let de: DebugElement;
+  let comp: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomePage],
+      imports: [
+        IonicModule.forRoot(HomePage)
+      ],
+      providers: [
+        NavController
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h2'));
+  });
+
+  it('should create component', () => expect(comp).toBeDefined());
+
+  it('should have expected <h2> text', () => {
+    fixture.detectChanges();
+    const h3 = de.nativeElement;
+    expect(h3.innerText).toMatch(/ionic/i,
+      '<h2> should say something about "Ionic"');
+  });
+});
